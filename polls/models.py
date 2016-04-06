e1da83068d066aef4226a8ca596130d651568251
vanelin <vano3231@gmail.com>
2016-04-06 10:27:48 +0300

    change __str__ to __unicode__

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __str__ on Python 3
         return self.question_text
 
     def was_published_recently(self):
@@ -23,5 +23,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __str__(self):              # __unicode__ on Python 2
+    def __unicode__(self):              # __str__ on Python 3
         return self.choice_text
