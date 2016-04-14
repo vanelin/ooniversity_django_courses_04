257b83d70a60ed594889294c283591fc5adf1ecf
Ivan Voloboev <vano3231@gmail.com>
2016-04-15 00:56:51 +0300

    check Django kz№ 5_2

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from courses.models import Course, Lesson
 
-admin.site.register(Course)
+class LessonInline(admin.TabularInline):
+    model = Lesson
+
+class CourseAdmin(admin.ModelAdmin):
+
+    '''
+        Admin View for Course
+    '''
+    list_display = ('name', 'short_description')
+    search_fields =('name',)
+    inlines = [LessonInline]
+
+admin.site.register(Course,  CourseAdmin)
 admin.site.register(Lesson)
--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,26 @@
 from django.contrib import admin
 from students.models import Student
 
-admin.site.register(Student)
+
+def fullname(obj):
+    return ("%s %s" % (obj.name, obj.surname))
+fullname.short_description = 'full name'
+
+
+class StudentAdmin(admin.ModelAdmin):
+
+    '''
+        Admin View for Student
+    '''
+    list_display = (fullname, 'email', 'skype')
+    list_filter = ['courses']
+    search_fields = ['surname', 'email']
+    filter_horizontal = ('courses',) 
+    fieldsets = [
+        ('Personal info', {'fields': ['name', 'surname', 'date_of_birth']}),
+        ('Contact info', {'fields': ['email', 'phone', 'address', 'skype']}),
+        (None, {'fields': ['courses']}),
+        ]
+
+admin.site.register(Student, StudentAdmin)
+
