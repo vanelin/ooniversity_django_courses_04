5e2747f1310505f14bafc59c7f88c81985ea3510
Ivan Voloboev <vano3231@gmail.com>
2016-04-20 00:06:55 +0300

    check Django kz№7_2 (ver 2.0)

--- quadratic/forms.py
+++ quadratic/forms.py
@@ -6,15 +6,12 @@ from django import forms
 class QuadraticForm(forms.Form):
 
     """docstring for QuadraticForm"""
-    a = forms.IntegerField(label='коэффициент a', widget=forms.TextInput({"placeholder": "Enter integer"}))
-    b = forms.IntegerField(label='коэффициент b', widget=forms.TextInput({"placeholder": "Enter integer"}))
-    c = forms.IntegerField(label='коэффициент c', widget=forms.TextInput({"placeholder": "Enter integer"}))
+    a = forms.IntegerField(label='коэффициент a')
+    b = forms.IntegerField(label='коэффициент b')
+    c = forms.IntegerField(label='коэффициент c')
 
     def clean_a(self):
         data = self.cleaned_data['a']
         if data == 0:
             raise forms.ValidationError("коэффициент при первом слагаемом уравнения не может быть равным нулю")
-        # Always return the cleaned data, whether you have changed it or 
-        # not.
-        else:
-            return data
+        return data
--- quadratic/views.py
+++ quadratic/views.py
@@ -11,26 +11,22 @@ def quadratic_results(request):
     if request.GET:
         form = QuadraticForm(request.GET)
         if form.is_valid():
-            print form.cleaned_data
             a = form.cleaned_data['a']
             b = form.cleaned_data['b']
             c = form.cleaned_data['c']
             d = b**2-4*a*c  # discriminant
+            discriminant = "Дискриминант: %s" % (d)
             if d < 0:
-                result = "Дискриминант: %d \nДискриминант меньше нуля, квадратное уравнение не имеет действительных решений." % (
-                    d)
+                result = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
             elif d == 0:
                 x = (-b+math.sqrt(d))/2.0*a
-                result = "Дискриминант: %d \nДискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" % (
-                    d, round(x, 1))
+                result = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" % (round(x, 1))
             else:
                 x1 = float((-b+math.sqrt(d))/2*a)
                 x2 = float((-b-math.sqrt(d))/2*a)
-                result = "Дискриминант: %d \nКвадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" % (
-                    d, round(x1, 1), round(x2, 1))
-            return render(request, 'results.html', locals())
+                result = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" % (round(x1, 1), round(x2, 1))
+            return render(request, 'quadratic/results.html', locals())
         else:
-            return render(request, 'results.html', locals())
-    else:
-        form = QuadraticForm()
-        return render(request, 'results.html', {'form': form})
+            return render(request, 'quadratic/results.html', locals())
+    form = QuadraticForm()
+    return render(request, 'quadratic/results.html', {'form': form})
