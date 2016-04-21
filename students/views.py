6f06463f52c1823df72e52cd766aa873efc0b11c
Ivan Voloboev <vano3231@gmail.com>
2016-04-22 02:03:17 +0300

    check Django kz№7_3 (ver 3.0)

--- students/views.py
+++ students/views.py
@@ -22,40 +22,47 @@ def detail(request, student_id):
     return render(request, 'students/detail.html', {'students': student})
 
 
-def students_add(request):
+def create(request):
     if request.method == 'POST':
         form = StudentModelForm(request.POST)
         if form.is_valid():
             application = form.save()
-            msg = u'Student %s %s has been successfully added.' % (
-                application.name, application.surname)
+            msg = u"Student %s %s has been successfully added." % (application.name, application.surname)
             messages.success(request, msg)
             return redirect('students:list_view')
     else:
         form = StudentModelForm()
-    context = {'form': form}
-    return render(request, 'students/add.html', context)
+    return render(request, 'students/add.html', {'form': form})
 
 
-def students_edit(request, student_id):
+def edit(request, student_id):
     application = get_object_or_404(Student, id=student_id)
     if request.method == 'POST':
         form = StudentModelForm(request.POST, instance=application)
         if form.is_valid():
             form.save()
-            msg = u'Info on the student has been sucessfully changed.'
-            messages.success(request, msg)
+            messages.success(request, u"Info on the student has been sucessfully changed.")
     else:
         form = StudentModelForm(instance=application)
     return render(request, 'students/edit.html', {'form': form})
 
 
-def students_remove(request, student_id):
+def remove(request, student_id):
+    new_student = Student.objects.get(id=student_id)
+    if request.method == 'POST':
+        new_student.delete()
+        new_message = u"Info on %s %s has been sucessfully deleted." % (new_student.name, new_student.surname)
+        messages.success(request, new_message)
+        return redirect("students:list_view")
+    return render(request, "students/remove.html", {'name': new_student.name, 'surname': new_student.surname})
+
+
+def remove(request, student_id):
     application = get_object_or_404(Student, id=student_id)
     if request.method == 'POST':
         application.delete()
-        msg = u'Info on %s %s has been sucessfully deleted.' % (application.name, application.surname)
+        msg = u"Info on %s %s has been sucessfully deleted." % (application.name, application.surname)
         messages.success(request, msg)
         return redirect('students:list_view')
-    notice = u'The student %s %s will be removed' % (application.name, application.surname)
+    notice = u"The student %s %s will be removed" % (application.name, application.surname)
     return render(request, 'students/remove.html', {'notice': notice})
