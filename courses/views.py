2b301c8a309b3c8613464535c1dc677779de0763
Ivan Voloboev <vano3231@gmail.com>
2016-04-23 15:55:03 +0300

    check Django kz№8 (ver 2.0)

--- courses/views.py
+++ courses/views.py
@@ -31,8 +31,9 @@ def edit(request, course_id):
     if request.method == 'POST':
         form = CourseModelForm(request.POST, instance=application)
         if form.is_valid():
-            form.save()
+            application = form.save()
             messages.success(request, u"The changes have been saved.")
+            return redirect('courses:edit', application.id)
     else:
         form = CourseModelForm(instance=application)
     return render(request, 'courses/edit.html', {'form': form})
