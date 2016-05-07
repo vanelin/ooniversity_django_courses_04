a6f60fda0758cc059587090d8363faecc9b6a5a0
Ivan Voloboev <vano3231@gmail.com>
2016-05-08 01:20:48 +0300

    check Django kz№11.2 (ver 1.0)

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,120 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+from courses.models import Course, Lesson
+from coaches.models import Coach
+
+
+class CoursesListTest(TestCase):
+
+    """docstring for CoursesListTest"""
+
+    def test_list_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_title(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'PyBursa Description')
+
+    def test_course_create(self):
+        course = Course.objects.create(
+            name='PyBursa test course',
+            short_description='Web development with django')
+        response = self.client.get('/')
+        self.assertEqual(Course.objects.all().count(), 1)
+        self.assertEqual(response.status_code, 200)
+
+    def test_Template(self):
+        course = Course.objects.create(
+            name='PyBursa test course',
+            short_description='Web development with django')
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_link_remove_edit_course(self):
+        course = Course.objects.create(
+            name='PyBursa test course',
+            short_description='Web development with django')
+        response = self.client.get('/')
+        self.assertContains(response, '/courses/edit/{}/'.format(course.id))
+        self.assertContains(response, '/courses/remove/{}/'.format(course.id))
+
+    def test_remove_course(self):
+        course = Course.objects.create(
+            name='PyBursa test course',
+            short_description='Web development with django')
+        self.assertEqual(Course.objects.all().count(), 1)
+        course.delete()
+        self.assertEqual(Course.objects.all().count(), 0)
+
+
+class CoursesDetailTest(TestCase):
+
+    """docstring for CoursesDetailTest"""
+
+    def test_url(self):
+        client = Client()
+        course = Course.objects.create(
+            name='PyBursa test course',
+            short_description='Web development with django')
+        response = client.get('/courses/1/')
+        self.assertTemplateUsed(response, 'courses/detail.html')
+
+    def test_detail_item(self):
+        client = Client()
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 404)
+        course = Course.objects.create(
+            name='PyBursa test course',
+            short_description='Web development with django')
+        response = client.get('/courses/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_model_relation(self):
+        course = Course.objects.create(name='test1')
+        lesson = Lesson(course=Course.objects.create(name='test2'),
+                        subject='test1',
+                        description='test1',
+                        order=int('1')
+                        )
+        lesson.full_clean()  # `course` correctly set. This should pass
+        lesson.save()
+        self.assertEqual(
+            Lesson.objects.filter(course__name='test2').count(), 1)
+
+    def test_model_relation_course_missing(self):
+        course = Course.objects.create(name='test1')
+        lesson = Lesson()  # Lesson without `course` set
+        with self.assertRaises(ValidationError):
+            lesson.full_clean()
+            lesson.save()
+        self.assertEqual(
+            Lesson.objects.filter(course__name='test2').count(), 0)
+
+    def test_coach(self):
+        client = Client()
+        coach = Coach.objects.create(
+            user=User.objects.create_user(
+                username='coach_1',
+                password='qwerty',
+                first_name=u'Ivan',
+                last_name=u'Ivanov',
+                email='coach_1@coach.com'),
+            date_of_birth='1988-01-01',
+            gender='M',
+            phone='1234657980',
+            address='Mykolayiv',
+            skype='ivan',
+            description='admin',)
+        course = Course.objects.create(name='PyBursa test course',
+                                       short_description='Web development with django',
+                                       coach=Coach.objects.get(id=1)
+                                       )
+        response = client.get('/courses/1/')
+        self.assertContains(response, 'Ivan Ivanov')
+        self.assertEqual(response.status_code, 200)
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,151 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from students.models import Student
+from courses.models import Course
+
+
+class StudentsListTest(TestCase):
+
+    def test_index_1(self):
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_2(self):
+        response = self.client.get('/students/')
+        self.assertTrue('student_list' in response.context)
+
+    def test_view_title(self):
+        response = self.client.get('/students/')
+        self.assertContains(response, 'PyBursa students list')
+
+    def test_Entry_Fields_Links(self):
+        course = Course.objects.create(
+            name='Django Base',
+            short_description='Django Base Course')
+        course.save()
+        student = Student(
+            name="Ivan",
+            surname="Ivanov",
+            date_of_birth="1999-09-09",
+            email="test@test.com",
+            phone="1234567890",
+            address="Mykolyiv",
+            skype="test")
+        student.save()
+        student.courses.add(course)
+        response = self.client.get('/students/')
+        self.assertContains(response, 'Django Base')
+        self.assertContains(response, '/students/edit/{}/'.format(student.id))
+        self.assertContains(response, '/students/remove/{}/'.format(student.id))
+        self.assertContains(response, '/students/add/')
+        self.assertEqual(student.name, "Ivan")
+        self.assertEqual(student.date_of_birth, "1999-09-09")
+        self.assertEqual(course.name, 'Django Base')
+        self.assertEqual(student.courses.all()[0], course)
+
+    def test_pagination(self):
+        for name in ["test1", "test2", "test3", "test4"]:
+            Student.objects.create(
+                name=name,
+                surname="Ivanov",
+                date_of_birth="1999-09-09",
+                email="test@test.com",
+                phone="1234567890",
+                address="Mykolyiv",
+                skype="test")
+        response = self.client.get('/students/')
+        self.assertContains(response, 'next >>')
+
+    def test_student_remove(self):
+            student = Student.objects.create(
+                name='Ivan',
+                surname="Ivanov",
+                date_of_birth="1999-09-09",
+                email="test@test.com",
+                phone="1234567890",
+                address="Mykolyiv",
+                skype="test")
+            self.assertEqual(Student.objects.all().count(), 1)
+            student.delete()
+            self.assertEqual(Student.objects.all().count(), 0)
+
+
+class StudentsDetailTest(TestCase):
+
+    def test_index(self):
+        student = Student.objects.create(
+                name='Ivan',
+                surname="Ivanov",
+                date_of_birth="1999-09-09",
+                email="test@test.com",
+                phone="1234567890",
+                address="Mykolyiv",
+                skype="test")
+        response = self.client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Template_Title(self):
+        student = Student.objects.create(
+                name='Ivan',
+                surname="Ivanov",
+                date_of_birth="1999-09-09",
+                email="test@test.com",
+                phone="1234567890",
+                address="Mykolyiv",
+                skype="test")
+        response = self.client.get('/students/1/')
+        self.assertTemplateUsed(response, 'students/student_detail.html')
+        self.assertContains(response, 'Student detail - PyBursa')
+
+    def test_detail_item(self):
+        client = Client()
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 404)
+        student = Student.objects.create(
+                name='Ivan',
+                surname="Ivanov",
+                date_of_birth="1999-09-09",
+                email="test@test.com",
+                phone="1234567890",
+                address="Mykolyiv",
+                skype="test")
+        response = client.get('/students/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_link_course(self):
+        course = Course.objects.create(
+            name='Django Base',
+            short_description='Django Base Course')
+        course.save()
+        student = Student(
+            name="Ivan",
+            surname="Ivanov",
+            date_of_birth="1999-09-09",
+            email="test@test.com",
+            phone="1234567890",
+            address="Mykolyiv",
+            skype="test")
+        student.save()
+        student.courses.add(course)
+        response = self.client.get('/students/1/')
+        self.assertContains(response, '/courses/{}/'.format(student.id))
+        self.assertContains(response, 'Django Base')
+
+    def test_detail(self):
+        student = Student.objects.create(
+                name='Ivan',
+                surname="Ivanov",
+                date_of_birth="1999-09-09",
+                email="test@test.com",
+                phone="1234567890",
+                address="Mykolyiv",
+                skype="test")
+        response = self.client.get('/students/1/')
+        self.assertEqual(response.context['student'].pk, 1)
+        self.assertEqual(response.context['student'].email, 'test@test.com')
+
+        # Ensure that non-existent polls throw a 404.
+        response = self.client.get('/students/2/')
+        self.assertEqual(response.status_code, 404)
